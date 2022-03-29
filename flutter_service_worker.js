d47fc345aea98ca0eb932898311c9037d1623fc1
ekaraage <u.takuml10@gmail.com>
2022-03-29 15:04:59 +0900

    change description

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "be5a6498ef8ea54f610983ad131df429",
-"/": "be5a6498ef8ea54f610983ad131df429",
+"index.html": "19c524cd1fce90dbf1c1ac56757cf8c7",
+"/": "19c524cd1fce90dbf1c1ac56757cf8c7",
 "main.dart.js": "add0d6232f277934dfaa08fdc65f4a45",
 "manifest.json": "0dcfebee893de8ecf194ce9511510c60",
 "version.json": "1babe854379db2fff661f2b30d1f1c0c"
